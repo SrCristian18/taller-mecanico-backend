f3f1cf145ae7602b2705e8bd421f0f9a62675eca
SrCristian18 <carenascogollo@gmail.com>
2026-06-05 12:58:30 -0500

    Consumer añadido

---
+++ src/main/java/com/tallerMecanico/infrastructure/events/VehicleEventConsumer.java
@@ -0,0 +1,39 @@
+package com.tallerMecanico.infrastructure.events;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.tallerMecanico.domain.event.VehicleEvent;
+import com.tallerMecanico.infrastructure.persistence.entities.EventHistory;
+import com.tallerMecanico.infrastructure.persistence.repositories.EventHistoryJpaRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class VehicleEventConsumer {
+    private final EventHistoryJpaRepository repository;
+
+    @KafkaListener(
+        topics = "vehicle-events",
+        groupId = "vehicle-group"
+    )
+    public void consume(VehicleEvent event)
+    {
+        log.info("Evento recibido desde kafka -> {}",event);
+
+        EventHistory history = EventHistory.builder()
+        .eventType(event.getEventType())
+        .vehiclePlate(event.getVehiclePlate())
+        .description(event.getDescription())
+        .occurredAt(event.getOccurredAt())
+        .build();
+
+        repository.save(history);
+
+        log.info("Evento guardado en el historial");
+    }
+
+}
